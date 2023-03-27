e1c9afd7c9b140dbc94d66b15bfda9f2c58e97e2
PabloPicas0 <pawel.fe@gmail.com>
2023-03-27 04:37:17 +0200

    added users route and attached method to them

--- index.js
+++ index.js
@@ -8,12 +8,56 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI);
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  log: [
+    {
+      description: String,
+      duration: Number,
+      date: String,
+    },
+  ],
+});
+
+const userModel = mongoose.model("userModel", userSchema);
+
 app.use(cors());
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/users", (req, res) => {
+  const { username } = req.body;
+
+  new userModel({
+    username: username,
+  })
+    .save()
+    .then((doc) => {
+      res.json({ username: doc.username, _id: doc._id });
+    });
+});
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const { _id } = req.params;
+});
+
+app.get("/api/users", (req, res) => {
+  userModel.find({}).then((docs) => {
+    res.json(
+      docs.map((element) => {
+        return { username: element.username, _id: element._id };
+      })
+    );
+  });
 });
 
 const port = process.env.PORT || 3000;
